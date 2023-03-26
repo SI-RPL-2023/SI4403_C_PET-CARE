36bc4c36c18b1d622af76e0d5cca1b7a59d75357
rafikhairan <rafikhairan2632@gmail.com>
2023-03-26 11:29:10 +0700

    membuat model

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,28 +17,32 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
+    protected $guarded = [
+        'id'
     ];
 
+    public $timestamps = false;
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    // protected $hidden = [
+    //     'password',
+    //     'remember_token',
+    // ];
 
     /**
      * The attributes that should be cast.
      *
      * @var array<string, string>
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+
+    public function petOwner() {
+        return $this->hasOne(PetOwner::class);
+    }
+
+    public function vet() {
+        return $this->hasOne(Vet::class);
+    }
 }
