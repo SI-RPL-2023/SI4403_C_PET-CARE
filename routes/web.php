36bc7c6c56b21bdcda19ca1cddf4bec7c93915af
Diva <divaniazzanabilla@gmail.com>
2023-05-07 17:43:10 +0700

    membuat halaman book vet ref #14

--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,7 @@ Route::put('/myprofile', [ProfileController::class, 'update']);
 
 Route::get('/articles', [ArticleController::class, 'index']);
 Route::get('/articles/{article:slug}', [ArticleController::class, 'show']);
+
+Route::get('/vet', [AppointmentController::class, 'index']);
+Route::get('/vet/make-appointment/{vet:vet_id}', [AppointmentController::class, 'makeAppointment'])->middleware('auth');
+Route::post('/vet/make-appointment/{vet:vet_id}', [AppointmentController::class, 'store']);
